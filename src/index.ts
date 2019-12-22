8f84311d8129cd3475e05dd9c875506396658c79
Tom Stones <tom@tomstones.com.au>
2019-12-22 15:16:40 +1100

    ADDED | Stub for ToyRobotCli

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import { ToyRobotCli } from './libs/ToyRobotCli';
 const path: string = process.argv[2];
 
 if (path) {
-	console.log(path);
+	const cli: ToyRobotCli = new ToyRobotCli();
+	cli.open(path);
 } else {
 	console.log('Please specify a path');
 }
