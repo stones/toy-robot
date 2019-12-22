d30f336810b47684a51022e5c551e5bb1f47b088
Tom Stones <tom@tomstones.com.au>
2019-12-22 15:32:39 +1100

    ADDED | Stubbed CLI

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-// eslint-disable-next-line no-console
-console.log('Toy robot cli');
+import { ToyRobotCli } from './libs/Cli';
+
+const path: string = process.argv[2];
+
+if (path) {
+	const cli: ToyRobotCli = new ToyRobotCli();
+	cli.open(path);
+} else {
+	console.log('Please specify a path');
+}
---
+++ src/libs/Cli/Cli.ts
@@ -0,0 +1,6 @@
+export class ToyRobotCli {
+
+	public open(path: string): void {
+		console.log(`Opening ${path}`);
+	}
+}
