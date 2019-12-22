bea16a93bbf0680762261d60f1a9e25ee38c7de1
Tom Stones <tom@tomstones.com.au>
2019-12-22 20:54:48 +1100

    ADDED | ToyRobot service

--- src/libs/Cli/Cli.ts
+++ src/libs/Cli/Cli.ts
@@ -1,17 +1,21 @@
 import { Service } from 'typedi';
 import { FileReader } from '../FileReader';
 import { Store, ToyRobotState } from '../Store';
+import { ToyRobot } from '../ToyRobot';
 @Service()
 export class ToyRobotCli {
 
 	public constructor(
 		private readonly fileReader: FileReader,
-		private readonly store: Store
+		private readonly store: Store,
+		private readonly toyRobot: ToyRobot
 	) { }
 
 	public open(path: string): void {
 		this.fileReader.createStreamFromPath(path).subscribe((command: string) => console.log(command));
 
 		this.store.state$.subscribe((state: ToyRobotState) => console.log(state));
+
+		console.log(this.toyRobot);
 	}
 }
